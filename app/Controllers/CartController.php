4f69bf27cb44621a944afadea7b4429aa3f3a67f
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-29 17:10:55 +0530

    Implemented CartController with add to cart, delete from cart, view cart, cart status, and checkout functionalities

--- app/Controllers/CartController.php
+++ app/Controllers/CartController.php
@@ -9,23 +9,23 @@ use MongoDB\BSON\ObjectId;
 class CartController{
 
     private $dbObj;
-    private $cartDetails = [];
     
     public function __construct(){
         $this->dbObj = new CartProvider();
         $this->dbObjBook = new AddBookProvider();
         $this->dbObjCategory = new BookCategoryProvider();
+        $this->time =date_default_timezone_set('Asia/Kolkata');
+        $this->todayDateTimeIndia = date('Y-m-d');
     }
-  
+    
     public function addToCart($id, $quantity){
         $searchArray =[_id =>new ObjectId($id)];
         $result = $this->checkQuantityAvailable($id, $quantity);
         if($result == 0){
              return ["message" =>"quantity not available"];
         }
-  
         if($result==1) {
-               $result = $this->calculatePrice($id,$quantity);
+               $result = $this->checkPrice($id,$quantity);
                $data = $this->dbObjBook->findOne($searchArray);
                $name = $data['name'];
                $cartDetails = ["name" => $name, "quantity" => $quantity, "price" => $result];
@@ -33,7 +33,7 @@ class CartController{
                 return ["message"=>" added succusfull"];
            }
     }
-  
+    
     private function checkQuantityAvailable($id, $quantity) {
         $searchArray = [_id => new ObjectId($id)];
         $result = $this->dbObjBook->findOne($searchArray);
@@ -47,27 +47,24 @@ class CartController{
         }else{
             return 1;
         }
-            }
-  
-    private function calculatePrice($id, $quantity) {
+    }
+    
+    private function checkPrice($id, $quantity) {
         $searchArray = ['_id' => new ObjectId($id)];
         $quantity = $quantity['quantity'];
-        date_default_timezone_set('Asia/Kolkata');
-        $todayDateTimeIndia = date('Y-m-d');
         $result = $this->dbObjBook->findOne($searchArray);
         $seasonStartDate = $result[season_start_date];
         $seasonEndDate = $result[season_end_date];
-        $todayTimestamp = strtotime($todayDateTimeIndia);
+        $todayTimestamp = strtotime($this->todayDateTimeIndia);
         $seasonStartTimestamp = strtotime($seasonStartDate);
         $seasonEndTimestamp = strtotime($seasonEndDate);
-
         if ($todayTimestamp >= $seasonStartTimestamp && $todayTimestamp <= $seasonEndTimestamp) {
             return $result[season_price] * $quantity;
         } else {
             return $result[price] * $quantity;
         }
     }
-
+    
     public function deleteItemFromCart($id){
     $searchArray = ['_id'=>new ObjectId($id)];
     $result = $this->dbObj->deleteOne($searchArray);
@@ -93,4 +90,31 @@ class CartController{
         $result = $this->dbObj->recordCount($searchArray);
         return $result;
     }
+
+    public function checkOut(){
+        $data = $this->dbObj->find($searchArray = []);
+        if (!$data) {
+         return ["message" => "cart is empty"];
+        }
+        $result =$this->checkOutPrice($data);
+      return $result;
+    }
+
+    private  function checkOutPrice($data) {
+        $cartInfo = []; $totalPrice = 0;
+         foreach ($data as $value) {
+            $name = $value['name'];
+            $searchArray =["name" =>$name];
+            $result = $this->dbObjBook->findOne($searchArray);
+            $id = $result['_id'];
+            $quantity = $value['quantity'];
+            $result =$this->checkPrice($id,$quantity);
+            $cartInfo [] =["name" => $value['name'],"quantity" => $value['quantity'],"price" => $result];
+            $totalPrice +=$result;
+         }
+    
+        $cartInfo [] =["totalPrice" =>$totalPrice];
+        return $cartInfo;
+  }
 }
+

301c21f2ca9332a9250ad6f783fbf22ee21eee44
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-29 15:47:08 +0530

    Add RESTful API routes for  Cart

---
+++ app/api/CartApi.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\controllers;
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+
+$app = new App($configuration);
+
+$app->post('/cart/{id}' ,function (Request $request,Response $response){
+    $id = $request->getAttribute('id');
+    $parsedBody = $request->getParsedBody();
+    $addToCartController = new controllers\CartController();
+    $result = $addToCartController->addToCart($id,$parsedBody);
+    return $response->withStatus(200)->withJson($result);
+});
+
+$app->delete('/cart/{id}',function (Request $request,Response $response){
+    $id = $request->getAttribute(id);
+    $itemToDelete = new controllers\CartController();
+     $result =$itemToDelete->deleteItemFromCart($id);
+     return $response->withStatus(200)->withJson($result);
+
+});
+
+$app->get('/cart',function (Request $request,Response $response){
+    $viewCartItems = new controllers\CartController();
+    $result = $viewCartItems->viewCart();
+    return $response->withStatus(200)->withJson($result);
+});
+
+$app->get('/carts',function (Request $request,Response $response){
+    $veiwStatus = new controllers\CartController();
+    $result = $veiwStatus->cartStatus();
+    return $response->withStatus(200)->withJson($result);
+});
+$app->get('/cart/checkout',function (Request $request,Response $response){
+    $checkOutCart = new controllers\CartController();
+    $result = $checkOutCart->checkOut();
+    return $response->withStatus(200)->withJson($result);
+});
+
+$app->run();
+

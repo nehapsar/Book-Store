146fe78711da17dcfd9cca0b214fd22187b48c03
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-29 17:02:40 +0530

    Implemented CRUD operations and book search functionalities in AddBookController

--- app/Controllers/AddBookController.php
+++ app/Controllers/AddBookController.php
@@ -4,31 +4,38 @@ namespace App\controllers;
 use App\providers\AddBookProvider;
 use App\providers\BookCategoryProvider;
 use MongoDB\BSON\ObjectId;
+use MongoDB\BSON\Regex;
 use App\controllers\BookCategoryController;
 
 class AddBookController{
 
     private $dbObj;
+
     public function __construct(){
         $this->dbObj = new AddBookProvider();
         $this->dbBookObj = new BookCategoryProvider();
     }
 
-    public function addBooksToCategory($data) {
-        $validationResult = $this->isValidData($data);
-
-        if ($validationResult === true) {
-            $insertionResult = $this->dbObj->insertOne($data);
-            return ["result" => 'book added successfully'];
-        } else {
-            return $validationResult;
+    public function addBooksToCategory($data){
+       $searchArray=['_id'=>new ObjectId( $data['category_id'])];
+        $result = $this->dbBookObj->findOne($searchArray);
+        if (!$result) {
+            return ["message" => "category doesnot exist"];
         }
+
+        $validationResult = $this->isValidData($data);
+        if ($validationResult === true) {
+              $insertionResult = $this->dbObj->insertOne($data);
+              return ["result" => 'book added successfully'];
+         } else {
+                return $validationResult;
+            }
     }
 
     private function isValidData($data) {
-        if (!isset($data['name']) || !preg_match('/^[a-zA-Z]+$/', $data['name'])) {
+        if (!isset($data['name']) || !preg_match('/^[a-zA-Z\s]+$/', $data['name'])) {
             return ["message" => "invalid name"];
-        } elseif (!isset($data['author']) || !preg_match('/^[a-zA-Z]+$/', $data['author'])) {
+        } elseif (!isset($data['author']) || !preg_match('/^[a-zA-Z\s]+$/', $data['author'])) {
             return ["message" => "invalid author name"];
         } elseif (!isset($data['price']) || !is_int($data['price']) || $data['price'] == 0) {
             return ["message" => "invalid price"];
@@ -49,22 +56,19 @@ class AddBookController{
         } elseif (!isset($data['quantity']) || !is_int($data['quantity']) || $data['quantity'] < 1) {
             return ["message" => "invalid quantity"];
         }
-
-        return true; 
-    }
-
+        return true;
+     }
 
     public function updateBookInformation($id, $data) {
-
-            $searchArray = ["_id" => new ObjectId($id)];
-            $updateArray = ['$set' => $data];
-            $result = $this->dbObj->updateOne($searchArray, $updateArray);
-            if ($result) {
-                return ["result" => 'book updated successfully'];
-            } else {
-                return ["message" => "Failed to update book"];
-              }
-     }
+         $searchArray = ["_id" => new ObjectId($id)];
+         $updateArray = ['$set' => $data];
+         $result = $this->dbObj->updateOne($searchArray, $updateArray);
+         if ($result) {
+             return ["result" => 'book updated successfully'];
+         } else {
+             return ["message" => "Failed to update book"];
+           }
+    }
 
     public function book_details($id){
         $searchArray = ['_id' => new ObjectId($id)];
@@ -80,28 +84,20 @@ class AddBookController{
         $result = $this->dbObj->deleteOne($searchArray);
         return ["result" => $result];
     }
-
-   /* public function searchBookWithAlphabet($data){
-        $char_to_match = $data;
-        $regex_pattern = "/^" . preg_quote($char_to_match, '/') . "/i";
-        $searchArray = ['name' => ['$regex' => $regex_pattern, '$options' => 'i']];
-        $result = $this->dbObj->findOne($searchArray);
+  
+    public function searchBookWithAlphabet($data){
+        $searchArray = ["name" => ['$regex' => $data] ];
+        $result = $this->dbObj->find($searchArray);
         return $result;
-    }*/
-
+    }
+    
     public  function findBookWithCategoryName($id){
         $searchArray = ['_id'=> new ObjectId($id)];
         $result = $this->dbObj->findOne($searchArray);
-        $ouput =[];
-        $ouput["name"]=$result['name'];
-        $ouput["author"]  =$result['"author"'];
-        $ouput["quantity"]=$result['quantity'];
-        $ouput["season_start_date"]=$result['season_start_date'];
-        $ouput["season_end_date"]=$result['season_end_date'];
         $categoryId = $result['category_id'];
         $name=$this->findNameofCategory($categoryId);
-        $ouput['category_name']=$name;
-        return ['result' =>$ouput];
+        $output=['name'=>$result['name'],"author"=>$result['author'],"quantity" => $result['quantity'],"category"=>$name,"seson_start_date"=>$result['season_start_date'],"season_end_date"=>$result['season_end_date']];
+     return $output;
     }
 
     private function findNameofCategory($categoryId){
@@ -109,5 +105,8 @@ class AddBookController{
         $result = $this->dbBookObj->findOne($searchArray);
         $name= $result['name'];
         return  $name;
-   }
+     }
 }
+
+
+

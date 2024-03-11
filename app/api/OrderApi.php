95666e94a8ee1c2fa006efcc09e583d66b9cd130
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-03-11 14:26:48 +0530

    feat: Add Slim Framework routes for order-related actions with authentication checks

--- app/api/OrderApi.php
+++ app/api/OrderApi.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\controllers;
+use App\Middleware\AuthenticateAdmin;
+use App\Middleware\AuthenticateUser;
 use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -13,24 +15,47 @@ $configuration = [
 
 $app = new App($configuration);
 
-$app->delete('/order',function (Request $request,Response $response){
-    $cartItems = new controllers\OrderController();
-    $result = $cartItems->completeOrder();
-     return $response->withStatus(200)->withJson($result);
-});
+$app->group('/authenticate' , function ($app){
+    
+    $app->get('/order',function (Request $request,Response $response){
+        $parsedBody = $request->getParsedBody();
+        $viewAllItems = new controllers\OrderController();
+        $result = $viewAllItems->viewOrderList($parsedBody);
+        return $response->withStatus(200)->withJson($result);
+    });
 
-$app->get('/order',function (Request $request,Response $response){
-    $parsedBody = $request->getParsedBody();
-    $viewAllItems = new controllers\OrderController();
-    $result = $viewAllItems->viewOrderList($parsedBody);
-    return $response->withStatus(200)->withJson($result);
-});
+    $app->get('/order/{id}' , function (Request $request,Response $response){
+        $id = $request->getAttribute(id);
+        $viewOrderDetails =new controllers\OrderController();
+        $result = $viewOrderDetails->viewOrderDetails($id);
+        return $response->withStatus(200)->withJson($result);
+    });
 
-$app->get('/order/{id}' , function (Request $request,Response $response){
-    $id = $request->getAttribute(id);
-    $viewOrderDetails =new controllers\OrderController();
-    $result = $viewOrderDetails->viewOrderDetails($id);
-    return $response->withStatus(200)->withJson($result);
-});
+})->add(new AuthenticateAdmin());
+
+
+$app->group('/authenticates' , function ($app){
+
+    $app->get('/order',function (Request $request,Response $response){
+        $parsedBody = $request->getParsedBody();
+        $viewAllItems = new controllers\OrderController();
+        $result = $viewAllItems->viewOrderList($parsedBody);
+        return $response->withStatus(200)->withJson($result);
+    });
+
+    $app->get('/order/{id}' , function (Request $request,Response $response){
+        $id = $request->getAttribute('id');
+        $viewOrderDetails =new controllers\OrderController();
+        $result = $viewOrderDetails->viewOrderDetails($id);
+        return $response->withStatus(200)->withJson($result);
+    });
+
+    $app->delete('/orders',function (Request $request,Response $response){
+        $cartItems = new controllers\OrderController();
+        $result = $cartItems->completeOrder();
+        return $response->withStatus(200)->withJson($result);
+    });
+
+ })->add(new AuthenticateUser());
 
 $app->run();

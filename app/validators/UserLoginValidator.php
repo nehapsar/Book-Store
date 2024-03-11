6aebb800d72cdc5423ff33a21f62ab019f105eed
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-03-11 14:04:29 +0530

    feat: Implement UserLoginValidator for validating user login credentials

---
+++ app/validators/UserLoginValidator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\validators;
+
+use App\util\BaseValidator;
+
+class UserLoginValidator extends BaseValidator{
+
+    protected function collection(){
+        return "users";
+    }
+
+    protected function rules(){
+        return [
+            'login' => [
+                'email' =>['required' , 'email'],
+                'password' => ['required','string','min:8','max:12' ,'regex:/^(?=.*[A-Z])(?=.*[!@#$%^&*()-_=+{};:,<.>]){1,}(?=.*[0-9]){1,}/']
+            ]
+        ];
+
+    }
+}
